655115decaffda2d6203dab40ea3d1a4d1ae1b69
glndnyu <127914399+glndnyu@users.noreply.github.com>
2023-04-09 13:47:11 +0800

    Added game play body that runs the whole game when prompted in the console

--- script.js
+++ script.js
@@ -11,17 +11,41 @@ function getPlayerChoice(){
   if(playerChoice.toLowerCase() == "paper") return choices[1];
   if(playerChoice.toLowerCase() == "scissor") return choices[2];
   alert("Invalid Input");
+  return getPlayerChoice();
 }
-
-const computerSelection = getComputerChoice();
-const playerSelection = getPlayerChoice();
-
-console.log(computerSelection + " vs " + playerSelection);
 
 function playRound(playerSelection, computerSelection){
+  const win = "win";
+  const lose = "lose"
+  const tie = "tie";
 
+  if(playerSelection == computerSelection){
+    alert("You both chose " + playerSelection + ". This round's a tie!");
+    return tie;
+  }
+  if (playerSelection == "Rock"){
+    alert("You win this round!");
+    return win;
+  } 
+  alert("You lost this round");
+  return lose;
 }
 
-function game(){
+function playGame(){
+  let playerPoints = 0;
+  let computerPoints = 0;
+  let round = 1;
 
+  while (round <=5){
+    const computerSelection = getComputerChoice();
+    const playerSelection = getPlayerChoice();
+    const roundResult = playRound(playerSelection, computerSelection);
+    if (roundResult.includes("lose")) computerPoints++;
+    else if (roundResult.includes("win")) playerPoints++;
+    round++;
+  }
+
+  if(playerPoints == computerPoints) alert("It's a tie! Score is " + playerPoints + " & " + computerPoints);
+  else if (playerPoints > computerPoints) alert("You win! Score is " + playerPoints + " & " + computerPoints);
+  else alert("Computer wins! Score is " + playerPoints + " & " + computerPoints);
 }
