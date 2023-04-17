25b5c6e119e4485c875e8f3bed60e4aa32d9cc01
glndnyu <127914399+glndnyu@users.noreply.github.com>
2023-04-17 21:39:14 +0800

    Interact with the html elements while playing the game

--- script.js
+++ script.js
@@ -1,40 +1,73 @@
 const choices = ["Rock", "Paper", "Scissor"];
 let playerPoints = 0;
 let computerPoints = 0;
+const buttons = document.querySelectorAll("button");
+const h2 = document.querySelector("h2");
+const h4 = document.querySelector("h4");
+const playerScore = document.getElementById("playerScore");
+const computerScore = document.getElementById("computerScore");
+const playerImage = document.getElementById("player");
+const computerImage = document.getElementById("computer");
+
 function getComputerChoice(){
   const computerChoice = Math.floor(Math.random() * choices.length);
   return choices[computerChoice];
 }
 
+function playAnotherRound(){
+  if(confirm("Play another round?")){
+    playGame();
+  } else{
+    alert("Bye!");
+    document.documentElement.style.display = "none";
+  }
+}
+
 function checkWinner(){
   if(playerPoints == 5){ 
-    alert("You win!");
-    playGame();
+    //alert("You win!");
+    buttons.forEach(button => {
+      button.removeEventListener("click", playRound);
+    });
+    h2.textContent = "YOU WIN!";
+    h4.textContent = "Congrats 🎆!";
+    setTimeout(playAnotherRound, 500);
   } if(computerPoints == 5){
-    alert("You lose");
-    playGame();
+    buttons.forEach(button => {
+      button.removeEventListener("click", playRound);
+    });
+    h2.textContent = "YOU LOSE!";
+    h4.textContent = "Sorry 😔!";
+    setTimeout(playAnotherRound, 500);
   }
 }
 
 function playRound(event){
-  const playerSelection = event.target.textContent;
+  const playerSelection = event.target.alt;
   const computerSelection = getComputerChoice();
-  console.log(computerSelection);
+
+  playerImage.src = `images/${playerSelection}.png`;
+  computerImage.src = `images/${computerSelection}.png`;
   if(playerSelection == computerSelection){
-    alert("You both chose " + playerSelection + ". This round's a tie!");
-    console.log(`${playerPoints}, ${computerPoints}`);
+    //alert("You both chose " + playerSelection + ". This round's a tie!");
+    h2.textContent = "Round is a tie!";
+    h4.textContent = `Both chose ${playerSelection}`;
     return;
   }
   switch (true){
     case playerSelection == "Rock" && computerSelection == "Scissor":
     case playerSelection == "Paper" && computerSelection == "Rock":
     case playerSelection == "Scissor" && computerSelection == "Paper":
-      alert("You win this round! "+ playerSelection + " wins against "+computerSelection);
-      playerPoints++;
+      //alert("You win this round! "+ playerSelection + " wins against "+computerSelection);
+      h2.textContent = "You win this round!";
+      h4.textContent = `${playerSelection} wins against ${computerSelection}`;
+      playerScore.textContent = ++playerPoints
       break;
     default:
-      alert("You lose this round! "+ computerSelection + " wins against "+playerSelection);
-      computerPoints++;
+      //alert("You lose this round! "+ computerSelection + " wins against "+playerSelection);
+      h2.textContent = "You lose this round!";
+      h4.textContent = `${playerSelection} loses against ${computerSelection}`;
+      computerScore.textContent = ++computerPoints;
       break;
   }
   console.log(`${playerPoints}, ${computerPoints}`);
@@ -42,12 +75,21 @@ function playRound(event){
 }
 
 function playGame(){
-  playerPoints = 0;
-  computerPoints = 0;
-  const buttons = document.querySelectorAll("button");
+  initialize();
   buttons.forEach(button => {
   button.addEventListener("click", playRound);
   });
+}
+
+function initialize(){
+  playerPoints = 0;
+  computerPoints = 0;
+  h2.textContent = "Choose your hand";
+  h4.textContent = "First to score 5 points win the game";
+  playerScore.textContent = playerPoints;
+  computerScore.textContent = computerPoints;
+  playerImage.src = "";
+  computerImage.src = "";
 }
 
 playGame();
